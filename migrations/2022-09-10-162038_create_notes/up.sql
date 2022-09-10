bcf90737f214faa61fd506efc517fe88b6c412ce
zakinadhif <zaki.nadhif45@gmail.com>
2022-09-10 23:50:59 +0700

    Integrate diesel

---
+++ migrations/2022-09-10-162038_create_notes/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE notes (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR NOT NULL,
+	body TEXT NOT NULL
+)
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,49 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, Responder};
+use diesel::PgConnection;
+use diesel::r2d2::{self, ConnectionManager};
+use dotenvy::dotenv;
+
 use handlers::note;
 
+mod actions;
 mod handlers;
+
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+async fn api_banner() -> impl Responder {
+    "AlpineMD API"
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
-            web::scope("notes")
-                .route("", web::post().to(note::create_note))
-                .route("", web::get().to(note::index_notes))
-                .route("{id}", web::get().to(note::get_note))
-                .route("{id}", web::put().to(note::update_note))
-                .route("{id}", web::delete().to(note::delete_note)),
-        )
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be defined");
+    let bind_ip = std::env::var("BIND_IP").expect("BIND_IP must be defined");
+    let bind_port = std::env::var("BIND_PORT")
+        .expect("BIND_PORT must be defined")
+        .parse()
+        .expect("BIND_PORT must be a valid port");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .route("/", web::get().to(api_banner))
+            .service(
+                web::scope("notes")
+                    .route("", web::post().to(note::create_note))
+                    .route("", web::get().to(note::index_notes))
+                    .route("{id}", web::get().to(note::get_note))
+                    .route("{id}", web::put().to(note::update_note))
+                    .route("{id}", web::delete().to(note::delete_note))
+            )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((bind_ip, bind_port))?
     .run()
     .await
 }
